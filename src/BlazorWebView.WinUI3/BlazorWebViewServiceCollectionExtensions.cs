a5f1b4ca5278be592eb50c36f5d99241ab5ea1d2
Waheed Ahmad <ewammuslim@outlook.com>
2023-11-24 17:47:39 +0500

    added source files

---
+++ src/BlazorWebView.WinUI3/BlazorWebViewServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Wpf;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+	/// <summary>
+	/// Extension methods to <see cref="IServiceCollection"/>.
+	/// </summary>
+	public static class BlazorWebViewServiceCollectionExtensions
+	{
+		/// <summary>
+		/// Configures <see cref="IServiceCollection"/> to add support for <see cref="BlazorWebView"/>.
+		/// </summary>
+		/// <param name="services">The <see cref="IServiceCollection"/>.</param>
+		/// <returns>The <see cref="IServiceCollection"/>.</returns>
+		public static IWpfBlazorWebViewBuilder AddWpfBlazorWebView(this IServiceCollection services)
+		{
+			services.AddBlazorWebView();
+			services.TryAddSingleton(new BlazorWebViewDeveloperTools { Enabled = false });
+			services.TryAddSingleton(_ => new WpfBlazorMarkerService());
+			return new WpfBlazorWebViewBuilder(services);
+		}
+
+		/// <summary>
+		/// Enables Developer tools on the underlying WebView controls.
+		/// </summary>
+		/// <param name="services">The <see cref="IServiceCollection"/>.</param>
+		/// <returns>The <see cref="IServiceCollection"/>.</returns>
+		public static IServiceCollection AddBlazorWebViewDeveloperTools(this IServiceCollection services)
+		{
+			return services.AddSingleton<BlazorWebViewDeveloperTools>(new BlazorWebViewDeveloperTools { Enabled = true });
+		}
+	}
+}
---
+++ src/BlazorWebView.WinUI3/WpfDispatcher.cs
@@ -0,0 +1,99 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using CommunityToolkit.WinUI;
+using Microsoft.UI.Dispatching;
+using System;
+using System.Runtime.ExceptionServices;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNetCore.Components.WebView.Wpf
+{
+    internal sealed class WpfDispatcher : Dispatcher
+    {
+        private readonly DispatcherQueue _dispatcherQueue;
+
+        public WpfDispatcher(DispatcherQueue windowsDispatcher)
+        {
+            _dispatcherQueue = windowsDispatcher ?? throw new ArgumentNullException(nameof(windowsDispatcher));
+        }
+
+        public override bool CheckAccess()  => _dispatcherQueue.HasThreadAccess;
+
+        public override async Task InvokeAsync(Action workItem)
+        {
+            try
+            {
+                if (_dispatcherQueue.HasThreadAccess)
+                {
+                    workItem();
+                }
+                else
+                {
+                    await _dispatcherQueue.EnqueueAsync(workItem);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public override async Task InvokeAsync(Func<Task> workItem)
+        {
+            try
+            {
+                if (_dispatcherQueue.HasThreadAccess)
+                {
+                    await workItem();
+                }
+                else
+                {
+                    await _dispatcherQueue.EnqueueAsync(workItem);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public override async Task<TResult> InvokeAsync<TResult>(Func<TResult> workItem)
+        {
+            try
+            {
+                if (_dispatcherQueue.HasThreadAccess)
+                {
+                    return workItem();
+                }
+                else
+                {
+                    return await _dispatcherQueue.EnqueueAsync(workItem);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public override async Task<TResult> InvokeAsync<TResult>(Func<Task<TResult>> workItem)
+        {
+            try
+            {
+                if (_dispatcherQueue.HasThreadAccess)
+                {
+                    return await workItem();
+                }
+                else
+                {
+                    return await _dispatcherQueue.EnqueueAsync(workItem);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
